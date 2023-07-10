03eed4eca13e2e4d1368145ccca4a6522c9c0613
Wafermafer <m.fernanda.m.acosta@gmail.com>
2023-07-09 23:39:21 -0300

    renombrar variables

--- TP1 Algoritmo 1.4.cpp
+++ TP1 Algoritmo 1.4.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 int main()
 {
-  float num;
-  float total = 0;
-  int c = 0;
+  float userInput;
+  float acum = 0;
+  int contador = 0;
 
-  while (num >= 0)
+  while (userInput >= 0)
   {
     cout << "Ingrese un valor" << endl;
-    cin >> num;
-    total = total + num;
-    c = c + 1;
+    cin >> userInput;
+    acum = acum + userInput;
+    contador = contador + 1;
   }
-  cout << " La suma es:" << total << endl;
-  cout << "El promedio es:" << total / c << endl;
+  cout << " La suma es:" << acum << endl;
+  cout << "El promedio es:" << acum / contador << endl;
 
-  cin >> num;
+  cin >> userInput;
 
   return 0;
 }
